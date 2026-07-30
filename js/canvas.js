41b1a55e94a1079820b74458a051cf8e329dd6bb
yang-beta <yangbeta@gmail.com>
2026-07-30 16:48:42 +0800

    adding style

--- js/canvas.js
+++ js/canvas.js
@@ -34,11 +34,17 @@ window.addEventListener('resize',resizeCanvas);
 
 //上面宣告完成後，底下是實際進入繪製階段
 ctx.beginPath();
+// 1. 設定線條顏色（RGBA: 紅, 綠, 藍, 透明度）
+ctx.strokeStyle = 'rgb(215,160,80,0.8)';
+// 2. 設定線條粗細 (單位為像素 px)
+ctx.lineWidth = 6;
+// 3. 設定線條端點形狀 ('round' 代表圓潤端點)
+ctx.lineCap = 'round';
 ctx.moveTo(cx,0);
 ctx.lineTo(cx, canvas.height);
 ctx.stroke();
 
-//ctx.beginPath();
+ctx.beginPath();
 ctx.moveTo(0, canvas.height * 0.75);
 ctx.lineTo(cx * 2, canvas.height * 0.75);
 ctx.stroke();
