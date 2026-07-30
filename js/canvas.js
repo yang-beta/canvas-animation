967de0d26598ad4a560489ebc966a4794cd6fcac
yang-beta <yangbeta@gmail.com>
2026-07-30 11:47:58 +0800

    test

--- js/canvas.js
+++ js/canvas.js
@@ -1,6 +1,6 @@
 // js/canvas.js
 
-const canvas = document.getElementById('CanvasAnime123');
+const canvas = document.getElementById('CanvasAnime');
 
 //2. 向 canvas 索取 2D 繪圖畫筆
 const ctx = canvas.getContext('2d');
