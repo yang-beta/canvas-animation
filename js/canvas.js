70dbf6847ff1156ca161ded2f91a65afabb73b35
yang-beta <yangbeta@gmail.com>
2026-07-30 15:21:51 +0800

    測試繪製線條

--- js/canvas.js
+++ js/canvas.js
@@ -29,5 +29,13 @@ function resizeCanvas() {
 
 // 頁面初次載入時執行一次
 resizeCanvas();
-
+// 當使用者拖拉改變視窗大小時，自動觸發重新計算
 windows.addEventListener('resize',resizeCanvas);
+
+//上面宣告完成後，底下是實際進入繪製階段
+ctx.beginPath();
+ctx.moveTo(cx,0);
+ctx.lineTo(cx, canvas.height;
+ctx.stroke();
+)
+
