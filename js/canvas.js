8e706a68090b7ffc9928ebdf965fcda55c7ceb05
yang-beta <yangbeta@gmail.com>
2026-07-31 14:18:23 +0800

    test

--- js/canvas.js
+++ js/canvas.js
@@ -32,41 +32,69 @@ resizeCanvas();
 // 當使用者拖拉改變視窗大小時，自動觸發重新計算
 window.addEventListener('resize',resizeCanvas);
 
-function drawSingleBeam(){
+// function drawSingleBeam(){
 
-    // 1. 儲存當前的畫筆狀態
-    ctx.save();
-    // 2. 設定發光/陰影屬性
-    ctx.shadowColor ='rgba(215,160,80,0.6)';
-    ctx.shadowBlur = 25;
-    //上面宣告完成後，底下是實際進入繪製階段
-    ctx.beginPath();
-    // 1. 設定線條顏色（RGBA: 紅, 綠, 藍, 透明度）
-    ctx.strokeStyle = 'rgba(235,190,110,0.6)';
-    // 2. 設定線條粗細 (單位為像素 px)
-    ctx.lineWidth = 6;
-    // 3. 設定線條端點形狀 ('round' 代表圓潤端點)
-    ctx.lineCap = 'round';
-    ctx.moveTo(cx,0);
-    ctx.lineTo(cx, canvas.height*0.75);
-    ctx.stroke();
-    // 4. 還原畫筆狀態 (取消 shadowBlur 避免影響後續繪圖)
-    ctx.restore();
-}
+//     // 1. 儲存當前的畫筆狀態
+//     ctx.save();
+//     // 2. 設定發光/陰影屬性
+//     ctx.shadowColor ='rgba(215,160,80,0.6)';
+//     ctx.shadowBlur = 25;
+//     //上面宣告完成後，底下是實際進入繪製階段
+//     ctx.beginPath();
+//     // 1. 設定線條顏色（RGBA: 紅, 綠, 藍, 透明度）
+//     ctx.strokeStyle = 'rgba(235,190,110,0.6)';
+//     // 2. 設定線條粗細 (單位為像素 px)
+//     ctx.lineWidth = 6;
+//     // 3. 設定線條端點形狀 ('round' 代表圓潤端點)
+//     ctx.lineCap = 'round';
+//     ctx.moveTo(cx,0);
+//     ctx.lineTo(cx, canvas.height*0.75);
+//     ctx.stroke();
+//     // 4. 還原畫筆狀態 (取消 shadowBlur 避免影響後續繪圖)
+//     ctx.restore();
+// }
 
-drawSingleBeam();
+// drawSingleBeam();
 
-// ctx.beginPath();
-// ctx.strokeStyle = 'rgba(255,255,255,0.4)';
-// ctx.lineWidth = 60;
-// ctx.moveTo(0, canvas.height * 0.75);
-// ctx.lineTo(cx * 2, canvas.height * 0.75);
-// ctx.stroke();
+ctx.save(); 
 
-// ctx.beginPath();
-// ctx.strokeStyle = 'rgba(255,255,255,0.4)';
-// ctx.lineWidth = 60;
-// ctx.moveTo(0, canvas.height * 0.75);
-// ctx.lineTo(cx * 2, canvas.height * 0.75);
-// ctx.stroke();
+ctx.shadowColor = 'rgba(255, 240, 200, 0.9)'; 
+ctx.shadowBlur = 35;
+ctx.beginPath();
+ctx.lineWidth = 10;
+ctx.strokeStyle = 'rgba(255, 250, 230, 0.9)';
+ctx.lineCap = 'round';
+ctx.moveTo(cx, 0);
+ctx.lineTo(cx, canvas.height * 0.65);
+ctx.stroke();
+
+ctx.restore();
+
+ctx.save(); 
+
+ctx.shadowColor = 'rgba(215, 160, 80, 0.5)'; 
+ctx.shadowBlur = 15;
+ctx.beginPath();
+ctx.lineWidth = 4;
+ctx.strokeStyle = 'rgba(215, 160, 80, 0.5)';
+ctx.lineCap = 'round';
+ctx.moveTo(cx -40, 0);
+ctx.lineTo(cx -40, canvas.height * 0.65);
+ctx.stroke();
+
+ctx.restore();
+
+ctx.save(); 
+
+ctx.shadowColor = 'rgba(215, 160, 80, 0.5)'; 
+ctx.shadowBlur = 15;
+ctx.beginPath();
+ctx.lineWidth = 4;
+ctx.strokeStyle = 'rgba(215, 160, 80, 0.5)';
+ctx.lineCap = 'round';
+ctx.moveTo(cx +40, 0);
+ctx.lineTo(cx +40, canvas.height * 0.65);
+ctx.stroke();
+
+ctx.restore();
 
